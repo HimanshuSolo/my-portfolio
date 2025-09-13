76b7ffc5818e19915f97119b290f3371cd26c67d
HimanshuSolo2005 <himanshu.pv.verma@gmail.com>
2025-09-13 23:44:39 +0530

    more-dump

--- src/components/TextGenerateEffect.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { TextGenerateEffect } from "@/components/ui/text-generate-effect"
-
-const words = `Hello, I'm Himanshu, Full-stack developer & 3rd Year BTech Student.`;
-
-export function TextGenerateEffectDemo() {
-  return <TextGenerateEffect words={words} />;
-}
