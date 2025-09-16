98442ab2b24ac57917a1d6ec731944414f0a2797
HimanshuSolo2005 <himanshu.pv.verma@gmail.com>
2025-09-16 17:43:21 +0530

    all-link-updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Dashboard from '@/components/Dashboard';
 import './globals.css'
 
 const navigation = [
-  { name: 'Resume', href: 'https://drive.google.com/file/d/1aapBSER8uxLN_e7HNMf2n9OEASSfcxzX/view?usp=sharing' },
+  { name: 'Resume', href: 'https://drive.google.com/file/d/1smWEpg08BPjBdWc0a387fWGr0dDJzbc9/view?usp=sharing' },
   { name: 'Projects', href: 'projects' },
   { name: 'Achievements', href: 'achievements' },
   { name: 'Contact', href: 'contact' },
