40644cc2efb53234908d74af3d891a2442bb7836
HimanshuSolo2005 <himanshu.pv.verma@gmail.com>
2025-09-16 21:07:37 +0530

    links

--- src/components/contactFloat.tsx
+++ src/components/contactFloat.tsx
@@ -41,7 +41,7 @@ export function FloatingDockDemo() {
             icon: (
                 <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
             ),
-            href: "https://github.com/HimanshuSolo2005",
+            href: "https://github.com/HimanshuSolo",
         },
         {
             title: "LeetCode",
