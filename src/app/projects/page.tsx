7a00b4bafec0f71359ecb9c3cf99ba0af1f5dae7
HimanshuSolo2005 <himanshu.pv.verma@gmail.com>
2025-09-16 17:43:10 +0530

    all-link-updates

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,7 +10,7 @@ import { TimelineDemo } from '@/components/timeline-Comp';
 
 const navigation = [
   { name: 'Home', href: '/' },
-  { name: 'Resume', href: 'https://drive.google.com/file/d/1aapBSER8uxLN_e7HNMf2n9OEASSfcxzX/view?usp=sharing' },
+  { name: 'Resume', href: 'https://drive.google.com/file/d/1smWEpg08BPjBdWc0a387fWGr0dDJzbc9/view?usp=sharing' },
   { name: 'Projects', href: 'projects' },
   { name: 'Achievements', href: 'achievements' },
   { name: 'Contact', href: 'contact' },
